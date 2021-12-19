6e3b7458950b2c3d93f532ff2e61ceeec199c99d
Brayden Jo <brayden.jo@outlook.com>
2021-12-19 19:38:06 +0900

    5분 거래 중지, 1시간 시장가 매수 금지 대응 코드 추가

--- upbit.py
+++ upbit.py
@@ -17,6 +17,30 @@ krw_balance = upbit.get_balance(ticker="KRW")
 prev_tickers = pyupbit.get_tickers()
 time.sleep(DELAY)
 
+
+def buy_market_order(ticker, krw_balance):
+    # 오더북 조회
+    orderbook = pyupbit.get_orderbook(ticker)
+    orderbook_units = orderbook['orderbook_units']
+    order0 = orderbook_units[0]
+    ask0_price = order0['ask_price']      # 매도 1호가 가격
+
+    # 지정가 매수 
+    volume = int(krw_balance / ask0_price)
+    upbit.buy_limit_order(ticker, ask0_price, volume)
+
+
+def sell_market_order(ticker, volume):
+    # 오더북 조회
+    orderbook = pyupbit.get_orderbook(ticker)
+    orderbook_units = orderbook['orderbook_units']
+    order0 = orderbook_units[0]
+    bid0_price = order0['bid_price']      # 매도 1호가 가격
+
+    # 지정가 매도 
+    upbit.sell_limit_order(ticker, bid0_price, volume)
+
+
 while True:
     now = datetime.datetime.now()
     try:
@@ -35,7 +59,11 @@ while True:
         print("신규 상장: ", ticker)
 
         # 신규상장 암호화폐 시장가 매수
-        upbit.buy_market_order(ticker, krw_balance)
+        print("신규 상장 5분 대기 중")
+        time.sleep(60 * 5)          # 신규 상장 후 5분 거래 안됨 
+
+        # 오더북 조회 후 매도 1호가 가격으로 지정가 주문
+        buy_market_order(ticker, krw_balance)
         time.sleep(DELAY)
 
         coin_balance = upbit.get_balance(ticker)
@@ -48,7 +76,8 @@ while True:
             # Trailing stop
             if (curr_price < (high_price * TS_MARGIN)):
                 # 고점대비 설정값 만큼 하락시 매도 
-                upbit.sell_market_order(ticker, coin_balance)
+                # 오더북 조회 후 매수 1호가 가격으로 지정가 주문
+                sell_market_order(ticker, coin_balance)
                 break
 
             high_price = max(high_price, curr_price)
